16c67f834a2e04fe514324f62ef1f3ed2e345f4e
tinghaoK7727 <tinghao.kao@edhec.com>
2022-03-02 15:24:48 +0100

    Labs upload

---
+++ Module_1/Lab_18/Lab_18.sql
@@ -0,0 +1,10 @@
+SELECT * FROM apple.applelab;
+select distinct prime_genre from apple;
+select max(prime_genre) from apple where rating_count_ver is not null;
+select max(prime_genre) from apple where track_name is not null;
+select min(prime_genre) from apple where track_name is not null;
+select track_name, rating_count_tot, prime_genre, price from apple order by rating_count_tot desc limit 10;
+select track_name, user_rating, prime_genre, price from apple order by user_rating desc limit 10;
+select track_name, rating_count_tot, user_rating, prime_genre, price from apple where user_rating = 5 order by rating_count_tot desc limit 3;
+select track_name, rating_count_tot, user_rating, prime_genre, price from apple order by price;
+select track_name, rating_count_tot, user_rating, prime_genre, price from apple order by rating_count_tot desc;
